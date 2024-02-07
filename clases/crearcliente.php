5495d25a886e17dfa0df0b03b7f6ab246203d9ff
Ed2902 <ebernal@uniemoresarial.edu.co>
2024-02-07 16:01:22 -0500

    ruta

--- clases/crearcliente.php
+++ clases/crearcliente.php
@@ -19,31 +19,35 @@ $nombre = $_POST['nombre'];
 $representante_legal = $_POST['representantelegal'];
 $telefono = $_POST['telefono'];
 $direccion = $_POST['direccion'];
-$camara_comercio = $_FILES['camara']['name'];
-$rut = $_FILES['rut']['name'];
-$cc_representante = $_FILES['cc']['name'];
-$certificacion_comercial = $_FILES['comercial']['name'];
-$certificacion_bancaria = $_FILES['bancaria']['name'];
-$circular_170 = $_FILES['circular']['name'];
-$acuerdos_seguridad = $_FILES['seguridad']['name'];
-$estados_financieros = $_FILES['financieros']['name'];
-$autorizacion_tratamiento_datos = $_FILES['autorizacion']['name'];
-$visita = $_FILES['visita']['name'];
-$antecedentes_judiciales = $_FILES['antecedentes']['name'];
 $fecha_registro = $_POST['fecha'];
 
+// Rutas de los archivos
+$targetDirectory = "../guardar/";
+
+$camara_comercio = $targetDirectory . $_FILES['camara']['name'];
+$rut = $targetDirectory . $_FILES['rut']['name'];
+$cc_representante = $targetDirectory . $_FILES['cc']['name'];
+$certificacion_comercial = $targetDirectory . $_FILES['comercial']['name'];
+$certificacion_bancaria = $targetDirectory . $_FILES['bancaria']['name'];
+$circular_170 = $targetDirectory . $_FILES['circular']['name'];
+$acuerdos_seguridad = $targetDirectory . $_FILES['seguridad']['name'];
+$estados_financieros = $targetDirectory . $_FILES['financieros']['name'];
+$autorizacion_tratamiento_datos = $targetDirectory . $_FILES['autorizacion']['name'];
+$visita = $targetDirectory . $_FILES['visita']['name'];
+$antecedentes_judiciales = $targetDirectory . $_FILES['antecedentes']['name'];
+
 // Mover archivos subidos a la carpeta deseada en el servidor
-move_uploaded_file($_FILES['camara']['tmp_name'], '../guardar/' . $camara_comercio);
-move_uploaded_file($_FILES['rut']['tmp_name'], '../guardar/' . $rut);
-move_uploaded_file($_FILES['cc']['tmp_name'], '../guardar/' . $cc_representante);
-move_uploaded_file($_FILES['comercial']['tmp_name'], '../guardar/' . $certificacion_comercial);
-move_uploaded_file($_FILES['bancaria']['tmp_name'], '../guardar/' . $certificacion_bancaria);
-move_uploaded_file($_FILES['circular']['tmp_name'], '../guardar/' . $circular_170);
-move_uploaded_file($_FILES['seguridad']['tmp_name'], '../guardar/' . $acuerdos_seguridad);
-move_uploaded_file($_FILES['financieros']['tmp_name'], '../guardar/' . $estados_financieros);
-move_uploaded_file($_FILES['autorizacion']['tmp_name'], '../guardar/' . $autorizacion_tratamiento_datos);
-move_uploaded_file($_FILES['visita']['tmp_name'], '../guardar/' . $visita);
-move_uploaded_file($_FILES['antecedentes']['tmp_name'], '../guardar/' . $antecedentes_judiciales);
+move_uploaded_file($_FILES['camara']['tmp_name'], $camara_comercio);
+move_uploaded_file($_FILES['rut']['tmp_name'], $rut);
+move_uploaded_file($_FILES['cc']['tmp_name'], $cc_representante);
+move_uploaded_file($_FILES['comercial']['tmp_name'], $certificacion_comercial);
+move_uploaded_file($_FILES['bancaria']['tmp_name'], $certificacion_bancaria);
+move_uploaded_file($_FILES['circular']['tmp_name'], $circular_170);
+move_uploaded_file($_FILES['seguridad']['tmp_name'], $acuerdos_seguridad);
+move_uploaded_file($_FILES['financieros']['tmp_name'], $estados_financieros);
+move_uploaded_file($_FILES['autorizacion']['tmp_name'], $autorizacion_tratamiento_datos);
+move_uploaded_file($_FILES['visita']['tmp_name'], $visita);
+move_uploaded_file($_FILES['antecedentes']['tmp_name'], $antecedentes_judiciales);
 
 // Insertar datos en la base de datos
 $sql = "INSERT INTO cliente (id_cliente, nombre, representantelegal, telefono, direccion, fecha_registro, camara_comercio, rut, cc_representante, certificacion_comercial, certificacion_bancaria, circular_170, acuerdos_seguridad, estados_financieros, autorizacion_tratamiento_datos, visita, antecedentes_judiciales) VALUES ('$nit', '$nombre', '$representante_legal', '$telefono', '$direccion', '$fecha_registro', '$camara_comercio', '$rut', '$cc_representante', '$certificacion_comercial', '$certificacion_bancaria', '$circular_170', '$acuerdos_seguridad', '$estados_financieros', '$autorizacion_tratamiento_datos', '$visita', '$antecedentes_judiciales')";
